600caa1dda6622bfb39b796ef192a9cf76e3f518
sokolrrustemi <141067973+sokolrrustemi@users.noreply.github.com>
2023-09-25 19:10:17 +0200

    Add files via upload

---
+++ src/testCase/TC_0401.java
@@ -0,0 +1,33 @@
+package testCase;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utility.BaseDriver;
+
+public class TC_0401 extends BaseDriver {
+    public static void main(String[] args) {
+
+        driver.get("https://www.akakce.com/");
+
+        WebElement loginBtn = driver.findElement(By.xpath("//*[@id=\"H_rl_v8\"]/a [2]"));
+        loginBtn.click();
+        delay(1);
+
+        WebElement emailLogin = driver.findElement(By.xpath("//*[@id='life']"));
+        emailLogin.sendKeys("hakan24dem@gmail.com");
+        delay(1);
+
+        WebElement passwordLogin = driver.findElement(By.xpath("//*[@id='lifp']"));
+        passwordLogin.sendKeys("hakan.%=24");
+        delay(1);
+
+        WebElement submit = driver.findElement(By.xpath("//*[@id='lfb' and @value='Giriş yap']"));
+        submit.click();
+
+        WebElement accountName = driver.findElement(By.cssSelector("[rel='nofollow'][href='/akakcem/']"));
+        Assert.assertEquals("Hakan", accountName.getText());
+
+        delayQuit();
+    }
+}
